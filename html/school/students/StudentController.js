957bad5b306a48d1f6e208e26a79bceaed87f918
shagie <turnmichael@gmail.com>
2018-05-13 22:37:23 -0500

    Update students
    Move array up one level of data
    Add Student info in view
    Update controller to filter based on id passed in

--- html/school/students/StudentController.js
+++ html/school/students/StudentController.js
@@ -1,6 +1,15 @@
 angular.module('myAPP')
-    .controller('StudentController', function ($scope, $routeParams) {
+    .controller('StudentController', function ($scope, $routeParams, $http) {
         $scope.message = 'Look! I am an about page.';
         $scope.params = $routeParams;
         $scope.studentId = $routeParams.sid;
+
+        if($scope.studentId > 0) {
+            $http.get("data/students.json")
+                .then(function(response) {
+                    $scope.student = response.data[0].filter(function (i, n) {
+                        return i.id == $scope.studentId;    // XXX switch to ===
+                    })
+                })
+        }
     });
