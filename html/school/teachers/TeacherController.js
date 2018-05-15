9afaa7d559f4784c57ca242f38a432dae5ef6f1a
shagie <turnmichael@gmail.com>
2018-05-14 22:44:31 -0500

    Submit based data fetch / update

--- html/school/teachers/TeacherController.js
+++ html/school/teachers/TeacherController.js
@@ -1,4 +1,15 @@
 angular.module('myAPP')
-    .controller('TeacherController', function ($scope) {
+    .controller('TeacherController', function ($scope, $routeParams, $http) {
         $scope.message = 'Teacher information';
+        $scope.params = $routeParams;
+
+        $scope.submit = function() {
+            if ($scope.text) {
+                $http.get("data/teachers/" + $scope.text + ".json")
+                    .then(function (response) {
+                            $scope.teacher = response.data;
+                        }
+                    )
+            }
+        };
     });
