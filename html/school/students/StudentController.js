8e923c4c2ea1b69216b5d843650857023cd79f41
shagie <turnmichael@gmail.com>
2018-05-14 19:20:17 -0500

    Move data around
    Students to a "one file with multiple data objects"
    Teachers to a "multiple files with one data object"

--- html/school/students/StudentController.js
+++ html/school/students/StudentController.js
@@ -5,7 +5,7 @@ angular.module('myAPP')
         $scope.studentId = $routeParams.sid;
 
         if($scope.studentId > 0) {
-            $http.get("data/students.json")
+            $http.get("data/students/students.json")
                 .then(function(response) {
                     $scope.student = response.data.filter(function (i, n) {
                         return i.id == $scope.studentId;    // XXX switch to ===
