a225de713ab6001a5f84cbeae1987c1ace1d643e
shagie <turnmichael@gmail.com>
2018-07-22 21:09:34 -0500

    Move app and db behind proxy / disable CORS

--- App/school/teachers/TeacherController.js
+++ App/school/teachers/TeacherController.js
@@ -8,7 +8,7 @@ angular.module('myAPP')
 
         $scope.submit = function() {
             if ($scope.text) {
-                $http.get("http://localhost:5984/teachers/" + $scope.text)
+                $http.get("/couch/teachers/" + $scope.text)
                     .then(function (response) {
                             $scope.teacher = response.data;
                         }
